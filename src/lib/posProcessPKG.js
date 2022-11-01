185bd34ee137270108594dc84b43d23c1086f717
Kruceo <rafa2015krueger@gmail.com>
2022-11-01 09:57:58 -0300

    fix: pkg.json bug fix

--- src/lib/posProcessPKG.js
+++ src/lib/posProcessPKG.js
@@ -1,17 +1,23 @@
 import fs, { existsSync, readFileSync, writeFileSync } from 'fs'
-export default function process(page)
-{
-    
+export default function process(page) {
+
     let target = page
-    let pkg = target.pkg
-    let outPkg
-    if(!pkg)return
-    if(existsSync('./out/package.json'))
-    {
-        outPkg = JSON.parse(readFileSync('./out/package.json','utf-8'))
+    let pkg = target.pkg ?? null
+    let outPkg = {
+        "dependencies": {
+        }
     }
-    Object.assign(pkg.dependencies,outPkg.dependencies)
-    writeFileSync('./out/package.json',JSON.stringify(target.pkg,null,2))
+
+    if (pkg == null) return target
+    if (existsSync('./out/package.json')) {
+        outPkg = JSON.parse(readFileSync('./out/package.json', 'utf-8'))
+
+    }
+
+    if (outPkg.dependencies != null) {
+        Object.assign(pkg.dependencies, outPkg.dependencies)
+    }
+    writeFileSync('./out/package.json', JSON.stringify(target.pkg, null, 2))
     target.pkg = pkg
     return target
 }
